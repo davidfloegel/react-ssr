d7d568fb2dbf682ba906cdc24da1b13247eafc1c
David Floegel <mail@davidfloegel.com>
2019-06-07 17:19:46 +0100

    try make build work

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,7 +5,7 @@ import LoadablePlugin from '@loadable/webpack-plugin';
 
 import { alias } from './shared.config';
 
-const DIST_PATH = path.resolve(__dirname, 'public/dist');
+const DIST_PATH = path.join(__dirname, 'public/dist');
 const production = process.env.NODE_ENV === 'production';
 const development =
   !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
